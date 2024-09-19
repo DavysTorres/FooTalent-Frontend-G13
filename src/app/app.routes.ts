ce89b01e63a0127c54cb888a40dca2711b27cbac
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-09-19 13:07:41 +0200

    estructura y diseño landing page creada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  // otras rutas aquí
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
