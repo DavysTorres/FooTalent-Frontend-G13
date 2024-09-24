07b51bc0a464fa2458657f46340c444b3cda83c1
Juanmita <juanmapimientae@gmail.com>
2024-09-24 14:25:25 -0500

    Commit de seguridad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent},
+    { path: 'register', component: RegisterComponent}, 
 ];
 
 @NgModule({
