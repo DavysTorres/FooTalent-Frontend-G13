1b5dc3ce865cbf434853f0ca3dc55675d4f3e4d2
Felipe00726 <felipe21palacio@gmail.com>
2024-09-26 13:16:37 -0500

    creacion componentes validar correo y reseteo de contraseña

---
+++ src/app/components/validacion-correo/validacion-correo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validacion-correo',
+  standalone: true,
+  imports: [],
+  templateUrl: './validacion-correo.component.html',
+  styleUrl: './validacion-correo.component.css'
+})
+export class ValidacionCorreoComponent {
+
+}
