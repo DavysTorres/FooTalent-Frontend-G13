6fd4d282f94395b5b4180c6749d926fd26bc5047
Felipe00726 <felipe21palacio@gmail.com>
2024-10-01 19:29:11 -0500

    vista nueva contraseña

--- src/app/components/validacion-correo/validacion-correo.component.ts
+++ src/app/components/validacion-correo/validacion-correo.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
-
+import { RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'app-validacion-correo',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLinkWithHref],
   templateUrl: './validacion-correo.component.html',
   styleUrl: './validacion-correo.component.css'
 })
