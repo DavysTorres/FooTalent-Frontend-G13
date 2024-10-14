fab5439ebba246d6ec96bb79fb0bd12d36929cfe
Juanmita <juanmapimientae@gmail.com>
2024-10-14 12:48:44 -0500

    Ajustes de vistas en aprendiz dashboard y vista de curso

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, RouterLinkWithHref],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/components/panel-de-control/panel-de-control.component.ts
+++ src/app/components/panel-de-control/panel-de-control.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLinkWithHref } from '@angular/router';
+import { Router, RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-panel-de-control',
