ac37d9989a8aab43cd12187c700d26ad0e508b3b
DavisTorres <davis.940@gmail.com>
2024-10-15 14:02:11 -0500

    se agrego import de environment  en los servicios

--- src/app/services/contenido.service.ts
+++ src/app/services/contenido.service.ts
@@ -1,12 +1,13 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContenidoService {
-  private apiUrl = environment.API_URL; // Cambia la URL según tu backend
+  private apiUrl = environment.API_URL; 
   private http = inject(HttpClient)
 
   constructor() {}
