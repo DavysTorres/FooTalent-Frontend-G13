8a521b4e508ddebe268fc62ef3ea4f5c6e24aa8b
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-10-15 21:10:45 +0200

    corregida importacion environment

--- src/app/services/contenido.service.ts
+++ src/app/services/contenido.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/enviroment';
 
 @Injectable({
   providedIn: 'root'
