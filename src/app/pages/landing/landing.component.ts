a023c5612bd6b5a06274aac45f5e2c711bf08a74
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-10-16 18:12:00 +0200

    añadido filtro en barra buscador de cursos e iconos funcionan como botones que navegan a la informacion del curso

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -2,13 +2,32 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
+  imports: [HeaderComponent, FooterComponent, RouterModule, FormsModule, CommonModule],
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  searchTerm: string = '';
+  courses = [
+    { id: 1, name: 'PHP y Bases de datos', img: '/assets/images/php.png' },
+    { id: 2, name: 'Diseño web', img: '/assets/images/html.png' },
+    { id: 3, name: 'Phyton', img: '/assets/images/phyton.png' },
+    { id: 4, name: 'Java', img: '/assets/images/java.png' },
+    { id: 5, name: 'Javascript', img: '/assets/images/javascript.png' },
+    { id: 6, name: 'C++', img: '/assets/images/c.png' },
+  ];
+  
+  filteredCourses = [...this.courses];  // Inicializa con todos los cursos
+
+  filterCourses() {
+    this.filteredCourses = this.courses.filter(course => 
+      course.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
