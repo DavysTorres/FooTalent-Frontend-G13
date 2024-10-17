111e57f8e02becd572fa33d7f1a93f09a68b7d36
Alba Fresnillo Arévalo <fresnilloarevaloalba@gmail.com>
2024-10-17 17:29:52 +0200

    carrusel navbar cursos

--- src/app/components/course-navbar/course-navbar.component.ts
+++ src/app/components/course-navbar/course-navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterLinkWithHref } from '@angular/router';
 
 @Component({
@@ -6,8 +6,41 @@ import { Router, RouterLinkWithHref } from '@angular/router';
   standalone: true,
   imports: [RouterLinkWithHref],
   templateUrl: './course-navbar.component.html',
-  styleUrl: './course-navbar.component.css'
+  styleUrl: './course-navbar.component.css',
 })
-export class CourseNavbarComponent {
+export class CourseNavbarComponent implements AfterViewInit {
+  @ViewChild('carouselInner', { static: false }) carouselInner!: ElementRef;
+  currentIndex: number = 0;
+  itemsToShow: number = 5;
+  totalItems: number = 0;
 
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    this.totalItems = this.carouselInner.nativeElement.children.length;
+  }
+
+  // Mover hacia la izquierda
+  prev(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateCarousel();
+    }
+  }
+
+  // Mover hacia la derecha
+  next(): void {
+    if (this.currentIndex < this.totalItems - this.itemsToShow) {
+      this.currentIndex++;
+      this.updateCarousel();
+    }
+  }
+
+  // Actualiza la posición del carrusel
+  updateCarousel(): void {
+    const carousel = this.carouselInner.nativeElement as HTMLElement;
+    const itemWidth = carousel.clientWidth / this.totalItems;
+    const translateX = -(itemWidth * this.currentIndex * this.itemsToShow);
+    carousel.style.transform = `translateX(${translateX}px)`;
+  }
 }
