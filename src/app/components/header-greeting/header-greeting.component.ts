1106173428d7017f72dd2ac0afc165b1c270b4ec
KevinSalinas <gabiand1.gs@gmail.com>
2024-10-30 17:48:15 -0300

    Modificado la parte de creación de cursos como docente y modificacion en tarjeta del header

--- src/app/components/header-greeting/header-greeting.component.ts
+++ src/app/components/header-greeting/header-greeting.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-greeting',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-greeting.component.html',
   styleUrl: './header-greeting.component.css'
 })
 export class HeaderGreetingComponent {
-  @Input() usuario: { nombre: string }= { nombre: '' };
+  @Input() usuario: { nombre: string, role:string }= { nombre: '', role:'' };
 }
