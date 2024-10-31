5e693f0a319791832fdd32362b29e95e37017950
KevinSalinas <gabiand1.gs@gmail.com>
2024-10-31 14:00:18 -0300

    Agregado funcionalidad "Eliminar" en tarjeta de docentes

--- src/app/components/curso-tarjeta/curso-tarjeta.component.ts
+++ src/app/components/curso-tarjeta/curso-tarjeta.component.ts
@@ -1,5 +1,8 @@
+import { Component, EventEmitter, Output, Input, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CursoService } from '../../services/curso.service';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,10 +10,42 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule,CommonModule],
   templateUrl: './curso-tarjeta.component.html',
-  styleUrl: './curso-tarjeta.component.css'
+  styleUrls: ['./curso-tarjeta.component.css']
+
 })
 export class CursoTarjetaComponent {
-  @Input() curso: any;  // Recibe el objeto curso
+  @Input() curso: any;
+  @Output() cursoEliminado = new EventEmitter<void>(); // Emisor de evento para notificar eliminación
+  loading = false;
 
-  constructor() { }
+  private dialog = inject(MatDialog);
+
+  constructor(private cursoService: CursoService) { }
+
+  eliminarCurso(idCurso: string): void {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '300px',
+      data: {
+        title: 'Confirmar eliminación',
+        message: '¿Estás seguro de que deseas eliminar este curso?'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.loading = true;
+        this.cursoService.eliminarCurso(idCurso).subscribe(
+          () => {
+            console.log('Curso eliminado');
+            this.cursoEliminado.emit(); // Notifica al componente padre
+            this.loading = false;
+          },
+          error => {
+            console.error('Error al eliminar el curso:', error);
+            this.loading = false;
+          }
+        );
+      }
+    });
+  }
 }
